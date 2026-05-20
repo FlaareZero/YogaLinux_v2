bf9255aa29407b8b83e28fb309521a1418fa7fa2
Mattia Rugna <60710669+FlaareZero@users.noreply.github.com>
2026-05-20 21:03:25 +0200

    Add flameshot to user apps installation

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,7 +9,7 @@ sed -i '/^\[main\]/a max_parallel_downloads=10' /etc/dnf/dnf.conf
 dnf -y install libvirt virt-manager qemu-kvm flatpak-builder wlr-randr iotop sysstat lxqt-openssh-askpass lxpolkit
 
 # User apps
-dnf -y install nautilus kitty mpv
+dnf -y install nautilus kitty mpv flameshot
 
 # OBS and fully-featured ffmpeg with nonfree components from rpm fusion
 dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
